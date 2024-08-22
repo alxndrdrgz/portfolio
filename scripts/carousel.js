c1b776b7e46d94ec8ec7ec0ba2c5d88d60bb442a
Alex Rodriguez-Sta. Ana <alexander.e.rsa@gmail.com>
2024-08-22 15:38:41 -0400

    everything works

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -12,3 +12,17 @@ prevButton.addEventListener('click', () => {
   index = (index - 1 + carousel.children.length) % carousel.children.length;
   carousel.style.transform = `translateX(-${index * 100}%)`;
 });
+
+const carousel2 = document.getElementById('carousel2');
+const nextButton2 = document.getElementById('next2');
+const prevButton2 = document.getElementById('prev2');
+
+nextButton2.addEventListener('click', () => {
+  index = (index + 1) % carousel2.children.length;
+  carousel2.style.transform = `translateX(-${index * 100}%)`;
+});
+
+prevButton2.addEventListener('click', () => {
+  index = (index - 1 + carousel2.children.length) % carousel2.children.length;
+  carousel2.style.transform = `translateX(-${index * 100}%)`;
+});
